822f376553381ce596744516ad62f39fe1c52a66
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2015-07-17 17:20:05 -0400

    page 137

---
+++ redis/chat.js
@@ -0,0 +1,32 @@
+var net = require('net');
+var redis = require('redis');
+
+var client = redis.createClient(6379, '127.0.0.1');
+
+var server = net.createServer(function(socket) {
+  var subscriber;
+  var publisher;
+
+  socket.on('connect', function() {
+    subscriber = redis.createClient();
+    subscriber.subscribe('main_chat_room');
+
+    subscriber.on('message', function(channel, message) {
+      socket.write('Channel ' + channel + ': ' + message);
+    });
+
+    publisher = redis.createClient();
+  });
+
+  socket.on('data', function(data) {
+    publisher.publish('main_chat_room', data);
+  });
+
+  socket.on('end', function() {
+    subscriber.unsubscribe('main_chat_room');
+    subscriber.end();
+    publisher.end();
+  });
+});
+
+server.listen(3000);
