6acec3d4a616af02d8c2bd26ddd16a085610708f
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2015-07-20 21:20:13 -0400

    sample app routes added

---
+++ sample-app/app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send('Hello Express');
+});
+
+app.get('/me', function(req, res) {
+	res.send('@dubbelsyrfun');
+});
+
+app.get('/who/:name?', function(req, res) {
+	var name = req.params.name;
+	res.send(name + ' was here');
+});
+
+app.get('/who/:name?/:title?', function(req, res) {
+	var name = req.params.name;
+	var title = req.params.title;
+	res.send(name + ' was here' + ', who is ' + title);
+});
+
+app.get('*', function(req, res) {
+	res.send('Bad route');
+});
+
+var server = app.listen(3000, function() {
+	console.log('Listening on port 3000');
+});
