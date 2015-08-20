35ca14b866b6aeab17f653ac4aea0fb847e043e4
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2015-08-19 21:20:31 -0400

    commit

--- sample-app/app.js
+++ sample-app/app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 app.get('/', function(req, res) {
-	res.send('Hello Express');
+	res.render('default', {title: 'Home',
+	users: ['Kristofer', 'Kaitlin', 'Pikachu']
+	});
 });
 
 app.get('/me', function(req, res) {
